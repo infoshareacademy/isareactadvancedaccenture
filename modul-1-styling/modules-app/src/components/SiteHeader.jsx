4eb2b741f892e0757d2c4923590c71564cd2c463
Kamil Richert <krichert@atlassian.com>
2024-11-21 10:57:04 +0100

    Fixed using multiple styles in modules app

--- modul-1-styling/modules-app/src/components/SiteHeader.jsx
+++ modul-1-styling/modules-app/src/components/SiteHeader.jsx
@@ -6,37 +6,35 @@ export const SiteHeader = () => (
       <h1 className={styles.logo}>Cahee</h1>
       <nav className={styles.nav}>
         <ul className={styles.nav__menu}>
-          <li
-            className={`${styles["nav__menu-item"]} ${styles["nav__menu-item-active"]}`}
-          >
+          <li>
             <a
-              className={(styles["nav__menu-a"], styles["nav__menu--active"])}
+              className={`${styles["nav__menu-a"]} ${styles["nav__menu--active"]}`}
               href="#"
             >
               Home
             </a>
           </li>
-          <li className={styles["nav__menu-item"]}>
+          <li>
             <a className={styles["nav__menu-a"]} href="#about">
               About
             </a>
           </li>
-          <li className={styles["nav__menu-item"]}>
+          <li>
             <a className={styles["nav__menu-a"]} href="#services">
               Services
             </a>
           </li>
-          <li className={styles["nav__menu-item"]}>
+          <li>
             <a className={styles["nav__menu-a"]} href="#gallery">
               Gallery
             </a>
           </li>
-          <li className={styles["nav__menu-item"]}>
+          <li>
             <a className={styles["nav__menu-a"]} href="#blog">
               Blog
             </a>
           </li>
-          <li className={styles["nav__menu-item"]}>
+          <li>
             <a className={styles["nav__menu-a"]} href="#contact">
               Contact
             </a>
