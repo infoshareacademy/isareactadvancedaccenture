c5d59200580c6f9666a50afbe36cac9e67936251
Kamil Richert <krichert@atlassian.com>
2024-11-21 11:13:58 +0100

    Styled components

--- modul-1-styling/styled-app/src/components/SiteHeader.jsx
+++ modul-1-styling/styled-app/src/components/SiteHeader.jsx
@@ -1,43 +1,75 @@
 import "./SiteHeader.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 900px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const Logo = styled.h1`
+  font-family: Pacifico, cursive;
+  font-weight: 400;
+  font-size: 50px;
+  margin: 0;
+`;
+
+const NavMenu = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  font-weight: 600;
+  padding: 0;
+  margin: 0;
+`;
+
+const Link = styled.a`
+  color: #262626;
+  text-decoration: none;
+  display: block;
+  border-top: 3px solid transparent;
+  padding-top: 5px;
+  transition: border-top-color 0.2s;
+  margin-right: 35px;
+  font-size: 16px;
+  border-top-color: ${(props) => (props.active ? "#ff6033" : "transparent")};
+  &:hover {
+    border-top-color: #ff6033;
+  }
+`;
 
 export const SiteHeader = () => (
   <header className="site-header">
-    <div className="container">
-      <h1 className="logo">Cahee</h1>
+    <Container>
+      <Logo>Cahee</Logo>
       <nav className="nav">
-        <ul className="nav__menu">
-          <li className="nav__menu-item nav__menu-item-active">
-            <a className="nav__menu-a nav__menu--active" href="#">
+        <NavMenu>
+          <li>
+            <Link active href="#">
               Home
-            </a>
+            </Link>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#about">
-              About
-            </a>
+          <li>
+            <Link href="#about">About</Link>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#services">
-              Services
-            </a>
+          <li>
+            <Link href="#services">Services</Link>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#gallery">
-              Gallery
-            </a>
+          <li>
+            <Link href="#gallery">Gallery</Link>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#blog">
-              Blog
-            </a>
+          <li>
+            <Link href="#blog">Blog</Link>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#contact">
-              Contact
-            </a>
+          <li>
+            <Link href="#contact">Contact</Link>
           </li>
-        </ul>
+        </NavMenu>
       </nav>
-    </div>
+    </Container>
   </header>
 );
