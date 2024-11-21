bf4fec1f52f4488997108e6f13a00d13efcccf05
Kamil Richert <krichert@atlassian.com>
2024-11-21 10:43:57 +0100

    CSS modules for SiteHeader

--- modul-1-styling/modules-app/src/components/SiteHeader.jsx
+++ modul-1-styling/modules-app/src/components/SiteHeader.jsx
@@ -1,38 +1,43 @@
-import "./SiteHeader.css";
+import styles from "./SiteHeader.module.css";
 
 export const SiteHeader = () => (
-  <header className="site-header">
-    <div className="container">
-      <h1 className="logo">Cahee</h1>
-      <nav className="nav">
-        <ul className="nav__menu">
-          <li className="nav__menu-item nav__menu-item-active">
-            <a className="nav__menu-a nav__menu--active" href="#">
+  <header className={styles["site-header"]}>
+    <div className={styles.container}>
+      <h1 className={styles.logo}>Cahee</h1>
+      <nav className={styles.nav}>
+        <ul className={styles.nav__menu}>
+          <li
+            className={`${styles["nav__menu-item"]} ${styles["nav__menu-item-active"]}`}
+          >
+            <a
+              className={(styles["nav__menu-a"], styles["nav__menu--active"])}
+              href="#"
+            >
               Home
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#about">
+          <li className={styles["nav__menu-item"]}>
+            <a className={styles["nav__menu-a"]} href="#about">
               About
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#services">
+          <li className={styles["nav__menu-item"]}>
+            <a className={styles["nav__menu-a"]} href="#services">
               Services
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#gallery">
+          <li className={styles["nav__menu-item"]}>
+            <a className={styles["nav__menu-a"]} href="#gallery">
               Gallery
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#blog">
+          <li className={styles["nav__menu-item"]}>
+            <a className={styles["nav__menu-a"]} href="#blog">
               Blog
             </a>
           </li>
-          <li className="nav__menu-item">
-            <a className="nav__menu-a" href="#contact">
+          <li className={styles["nav__menu-item"]}>
+            <a className={styles["nav__menu-a"]} href="#contact">
               Contact
             </a>
           </li>
