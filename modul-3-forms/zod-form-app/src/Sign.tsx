1cda75438561e9e987e2abe2a78672d7487fb816
Kamil Richert <krichert@atlassian.com>
2024-11-22 14:38:49 +0100

    Init forms sign

--- modul-3-forms/zod-form-app/src/Sign.tsx
+++ modul-3-forms/zod-form-app/src/Sign.tsx
@@ -7,8 +7,40 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({});
+
+type FormData = {
+  name: string;
+  surname: string;
+  number: string;
+  work: "employed" | "unemployed";
+  company?: string;
+  role?: string;
+};
 
 export const Sign = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
+  const getFieldProps = (fieldName: keyof FormData) => ({
+    ...register(fieldName),
+    error: !!errors[fieldName],
+    helperText: errors[fieldName]?.message,
+  });
+
   return (
     <Box
       component="form"
@@ -20,22 +52,23 @@ export const Sign = () => {
       }}
       noValidate
       autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <Typography variant="h4" marginBottom={5}>
         Sign to conference
       </Typography>
 
       <FormLabel>Name</FormLabel>
-      <TextField margin="normal" />
+      <TextField margin="normal" {...getFieldProps("name")} />
 
       <FormLabel>Surname</FormLabel>
-      <TextField margin="normal" />
+      <TextField margin="normal" {...getFieldProps("surname")} />
 
       <FormLabel>Number</FormLabel>
-      <TextField margin="normal" type="number" />
+      <TextField margin="normal" type="number" {...getFieldProps("number")} />
 
       <FormLabel>Work</FormLabel>
-      <Select>
+      <Select {...getFieldProps("work")}>
         <MenuItem value="employed">employed</MenuItem>
         <MenuItem value="unemployed">unemployed</MenuItem>
       </Select>
