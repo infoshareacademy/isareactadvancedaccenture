f0e5f9acac4d2889c8bd42a1431c12e42f13e69b
Kamil Richert <krichert@atlassian.com>
2024-11-22 14:24:39 +0100

    Confirm password

--- modul-3-forms/zod-form-app/src/Confirm.tsx
+++ modul-3-forms/zod-form-app/src/Confirm.tsx
@@ -1,6 +1,41 @@
 import { TextField, FormLabel, Box, Button, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+type FormData = {
+  password: string;
+  confirmPassword: string;
+};
+
+const schema = z
+  .object({
+    password: z.string().min(1),
+    confirmPassword: z.string().min(1),
+  })
+  .superRefine(({ password, confirmPassword }, ctx) => {
+    if (password !== confirmPassword) {
+      ctx.addIssue({
+        code: "custom",
+        path: ["confirmPassword"],
+        message: "It has to be the same as password",
+      });
+    }
+  });
 
 export const Confirm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
     <Box
       component="form"
@@ -12,16 +47,27 @@ export const Confirm = () => {
       }}
       noValidate
       autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <Typography variant="h4" marginBottom={5}>
         Log in
       </Typography>
 
       <FormLabel>Password</FormLabel>
-      <TextField margin="normal" />
+      <TextField
+        margin="normal"
+        {...register("password")}
+        error={!!errors.password}
+        helperText={errors.password?.message}
+      />
 
       <FormLabel>Confirm password</FormLabel>
-      <TextField margin="normal" />
+      <TextField
+        margin="normal"
+        {...register("confirmPassword")}
+        error={!!errors.confirmPassword}
+        helperText={errors.confirmPassword?.message}
+      />
 
       <Button type="submit">Send</Button>
     </Box>
