e4016a3b6398cc2f89ba773ccd5b5f9edeb32ad1
Kamil Richert <krichert@atlassian.com>
2024-11-29 11:23:19 +0100

    React hook form example

--- modul-6-optimisation/memo-app/src/ReactHookForm.jsx
+++ modul-6-optimisation/memo-app/src/ReactHookForm.jsx
@@ -1,10 +1,31 @@
+import { useRef } from "react";
+
 const NameInput = () => {
+  const name = useRef("");
+  const nameRef = useRef("");
+
+  const onChange = (e) => {
+    name.current = e.target.value;
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+
+    console.log({
+      name: name.current,
+      nameRef: nameRef.current.value,
+      nameFormData: data.get("name"),
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>ReactHookForm</h1>
       <label>
         Wpisz swoje imię:
-        <input name="name" type="text" />
+        <input name="name" ref={nameRef} onChange={onChange} type="text" />
       </label>
       <button type="submit">Wyślij</button>
     </form>
